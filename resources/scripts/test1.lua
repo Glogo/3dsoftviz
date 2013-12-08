3df8f006f52de8ca2e4dd1bbe09b777c34628af1
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2013-12-08 21:09:46 +0100

    Factorial script

--- resources/scripts/test1.lua
+++ resources/scripts/test1.lua
@@ -1,28 +1,11 @@
 -- defines a factorial function
 function fact(n) 
-  if fact(n) 
+  if n == 0 
     then return 1 
-    elseif n==0 then return n * fact(n-1) 
-    elseif x >5 then local a = 5
-    else return n    
+  else 
+    return n * fact(n-1)    
   end 
 end
-local function p()
-  print("enter a number:")
-end
+print("enter a number:")
 a = io.read("*number")        -- read a number
 print(fact(a))
-for i,j in 1,10 do 
-  print("a")
-end
-while a ~= 0 do
-  a = a - 1
-  print(a)
-end
-repeat 
-  print(a)
-  a = a + 1
-until a == 5
-do 
-  local i = i+1;
-end
